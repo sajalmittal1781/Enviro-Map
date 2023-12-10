f49542397c84448ce0221872f27d8cdd14f8c160
sajalmittal1781 <sajalmittal1781@gmail.com>
2023-12-11 01:21:48 +0530

    updated backend link

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -15,7 +15,7 @@ const Home = () => {
     useEffect(() => {
         const fetchCities = async () => {
             try {
-                const response = await axios.get('http://localhost:4000/cities'); 
+                const response = await axios.get('https://weather-et1t.onrender.com/cities'); 
                 setCities(response.data);
             } catch (error) {
                 console.error('Error fetching cities:', error);
